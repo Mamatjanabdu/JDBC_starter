aeba03788d636c5d411bad27e2c8a4c52ac7305c
mamatjanabdu <mouhanmadi@gmail.com>
2019-09-11 22:03:43 -0400

    new changes after build4

--- src/main/java/REst_Practice/SpartantRest_test.java
+++ src/main/java/REst_Practice/SpartantRest_test.java
@@ -1,14 +1,17 @@
 package REst_Practice;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class SpartantRest_test {
+
     @BeforeClass
     public static void setUp() {
         RestAssured.baseURI = "http://3.89.115.0";
@@ -23,17 +26,71 @@ public class SpartantRest_test {
 //    and  header should contains Date
     @Test
     public void Spartan_All_Test(){
+        //Response response = RestAssured.get("/spartans/");
         Response response = get("/spartans/");
-
         assertEquals(200, response.statusCode());
-        assertEquals("",response.contentType());
-        assertEquals("", response.getContentType());
-        //assertEquals("",response);
-
+        // below codes are doing same exact thing
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+        assertEquals("application/json;charset=UTF-8",response.getContentType());
+        assertEquals("application/json;charset=UTF-8",response.getHeader("content-type"));
+        // checking whether one header exists
+        // hasHeaderWithName is not case sensitive
+        boolean hasDateHeader = response.getHeaders().hasHeaderWithName("date");
+        assertTrue(hasDateHeader);
     }
-//    Given no headers are provided
+    //    Given no headers are provided
 //    When User send request to /api/spartans/2
 //    Then Response status code should be 200
 //    and header should have content Type / JSON
+//      and text Nels show up in response body
 //   OPTIONAL  and json object id should be 2
+    @Test
+    public void SingleSpartanData_Test(){
+        //1st way
+        //Response response = get("/spartans/2");
+        //2nd way
+        Response response =
+                //given().pathParam("id",2).get("/spartans/{id}")   ;
+                given().pathParam("id",2).when().get("/spartans/{id}");
+        System.out.println( response.asString()  );
+        System.out.println( response.body().asString()  );
+        // this will print result in formatted style
+        response.prettyPrint();
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+        assertTrue(  response.asString().contains("Nels")    );
+    }
+    //    Given Accept header is provided as Json
+//    When User send request to /api/spartans/2
+//    Then Response status code should be 200
+//    and header should have content Type / JSON
+//    and json object id should be 2
+    @Test
+    public void SingleSpartanDataWithHeader_Test(){
+        // RequestSpecification object hold the information about the request
+        // like header , path variable , query parameters, body
+        // Response is the object to store Response data
+        // this is how we can path header to teh request
+        Response response = given()
+                //.header("accept","application/json")
+                //.accept("application/json")
+                .accept(ContentType.JSON)
+                .when().get("/spartans/2");
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+
+    }
+    //    Given no header is provided
+//    When User send request to /api/spartans/20000
+//    Then Reponse status code should be 404
+//    and header should have content Type / JSON
+//    and response payload should contains "Spartan Not Found"
+    @Test
+    public void Invalid_Spartan_ID_should_return_404_Test(){
+        Response response =  //get("/spartans/20000");
+                given().pathParam("my_id",20000).when().get("/spartans/{my_id}");
+        response.prettyPrint();
+        assertEquals(404, response.statusCode());
+        assertEquals("application/json;charset=UTF-8",response.contentType());
+        assertTrue(  response.asString().contains("Spartan Not Found")    );
+    }
 }
