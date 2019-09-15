399b4e320b650f741d3aad6865fe95b927fa9c21
mamatjanabdu <mouhanmadi@gmail.com>
2019-09-15 11:53:38 -0400

    new changes after build4

--- src/test/java/RestPractice/Spartan_Rest_API_Sunday.java
+++ src/test/java/RestPractice/Spartan_Rest_API_Sunday.java
@@ -1,7 +1,9 @@
 package RestPractice;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import utils.ConfigurationReader;
 
 
@@ -19,5 +21,15 @@ public class Spartan_Rest_API_Sunday {
         RestAssured.basePath = ConfigurationReader.getProperty("spartan.base_path");
         // above will generate a BASE REQUEST URL OF http://52.23.254.102:8000/api
     }
+  @Test
+    public void All_Sparant(){
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/spartans/").prettyPeek()
+                .then()
+                .statusCode(200)
+                .body("".length()100);
 
+  }
 }
